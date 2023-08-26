51aa80ed6ad08751d39a0fb51d275429ecfea90c
Adedoyin <isradeoluwa@gmail.com>
2023-08-26 10:54:07 +0100

    #ARRAYS: Reversing arrays

---
+++ arrays/reverse string.js
@@ -0,0 +1,14 @@
+// My First Approach: USING DEFAULT JS FUNCTIONS
+
+// declare the string
+let string = "Hi My name is Adedoyin";
+
+// split the string to return and array of chars
+let myArrayOfChar = string.split("");
+
+// reverse the array of characters
+let myReversedArrayOfChar = myArrayOfChar.reverse();
+
+// Join the reverse array of characters and print it out
+let reversedString = myReversedArrayOfChar.join("");
+console.log(reversedString);
