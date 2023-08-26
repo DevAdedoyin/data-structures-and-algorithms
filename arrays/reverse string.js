67e2ac5f6d35bccfbd123155cf7276ac003ae20a
Adedoyin <isradeoluwa@gmail.com>
2023-08-26 11:12:26 +0100

    #ARRAYS: Reversing arrays of strings 2

--- arrays/reverse string.js
+++ arrays/reverse string.js
@@ -12,3 +12,25 @@ let myReversedArrayOfChar = myArrayOfChar.reverse();
 // Join the reverse array of characters and print it out
 let reversedString = myReversedArrayOfChar.join("");
 console.log(reversedString);
+
+////////////////////////////////////////////////////////
+
+// My second approach: MANUALLY, WITH FOR LOOP
+
+// declare the string variable
+let myString = "Hi My name is Adedoyin";
+
+// split the string to return an array of characters
+let splitMyString = myString.split("");
+
+// create an empty array of characters
+let arrayOfReversedChars = [];
+
+// Loop through the splitMyString array in a reversed order
+for (let position = splitMyString.length; position > 0; position--) {
+  // add items to the arrayOfReversedChars array
+  arrayOfReversedChars.push(splitMyString[position - 1]);
+}
+
+// convert to string with join method and print the result
+console.log(arrayOfReversedChars.join(""));
